4b24789c524ea89a2bf2e0a3b2a9b11246f0c8d9
Aditya Neralkar <neralkarad15.it@coep.ac.in>
2017-09-01 21:58:04 +0530

    few functions and .gitignore added

--- ana.py
+++ ana.py
@@ -1,7 +1,28 @@
-a = open('train.csv', 'r')
-labels =set() 
-for x in a:
-    image_name, label =  x.split(',')
-    print image_name, label[:-1]
-    labels.add(label[:-1])
-print len(labels)
+if __name__ == '__main__':
+    main()
+l = []
+dict = {}
+def main():
+    a = open('train.csv', 'r')
+    labels =set() 
+    for x in a:
+        image_name, label =  x.split(',')
+        #print image_name, label
+        labels.add(label[:-1])
+    labels.remove('label')
+    i = 0
+    for j in labels:
+        dict[j] = i
+        i += 1
+        l.append(j)
+def returndict():       #returns dictionary
+    return dict
+def returnlist():       #returns list
+    return l
+
+"""
+import ana
+ana.main()
+list = ana.returnlist()
+dict = ana.returndict()
+"""
