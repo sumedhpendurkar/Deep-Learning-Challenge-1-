aa8fd650d454c10cf87b64f42bcdc08e5cce781c
Sumedh Pendurkar <sumedh.pendurkar@gmail.com>
2017-09-07 13:02:16 +0530

    scaping and dumping into pickle

--- ana.py
+++ ana.py
@@ -1,3 +1,8 @@
+import cv2
+import numpy as np
+import os
+import pickle
+
 def generate_labelname_label_mapping():
     a = open('train.csv', 'r')
     l = []
@@ -16,5 +21,28 @@ def generate_labelname_label_mapping():
     return di, l
 
 
+
+def generate_training_set(direct):
+    image_name = os.listdir(direct)
+    mat = np.zeros((len(image_name), 256, 256, 3))
+    labels = np.zeros((len(image_name)))
+    a = open('train.csv', 'r')
+    a = a.readlines()
+    a = a[1:]
+    a.sort()
+    image_name.sort()
+    di, _ = generate_labelname_label_mapping()
+    i = 0
+    for imagename in image_name:
+        im = cv2.imread(direct+imagename)
+        mat[i] = im
+        labels[i] = di[a[i].split(',')[1][:-1]]
+        i+=1
+    return mat, labels
+
+def wrapper():
+    mat, label = generate_training_set('train_img/')
+    pickle.dump({'x':mat, 'label': label}, open("dataset","wb"))
+
 if __name__ == "__main__":
-    print generate_labelname_label_mapping()
+    wrapper()
